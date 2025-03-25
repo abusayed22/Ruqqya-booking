a04480e2cc4815b1591c47f60815e1ccc70727f9
abu sayed <abusayed.in22@gmail.com>
2025-03-25 11:59:00 +0600

    modal test

--- src/components/BookingRequest.jsx
+++ src/components/BookingRequest.jsx
@@ -117,7 +117,7 @@ const BookingRequest = () => {
                       type="button"
                       className="bg-warning-focus text-warning-main px-24 py-4 rounded-pill fw-medium text-sm"
                       data-bs-toggle="modal"
-                      data-bs-target="#exampleModal"
+                      data-bs-target="#exampleModalLong"
                     >
                       <Icon
                         icon="ph:read-cv-logo"
@@ -125,7 +125,55 @@ const BookingRequest = () => {
                       ></Icon>
                     </button>
                     {/* Modal Add Event */}
-                    <div
+                    
+
+
+<div className="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+  <div className="modal-dialog" role="document">
+    <div className="modal-content">
+      <div className="modal-header">
+        <h5 className="modal-title" id="exampleModalLongTitle">Modal title</h5>
+        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div className="modal-body">
+      Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.
+
+Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+
+Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+
+Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.
+
+Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+
+Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+
+Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.
+
+Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+
+Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+
+Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.
+
+Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+
+Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+
+Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.
+      </div>
+      {/* <div className="modal-footer">
+        <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" className="btn btn-primary">Save changes</button>
+      </div> */}
+    </div>
+  </div>
+</div>
+
+                    
+                    {/* <div
                       className="modal fade"
                       id="exampleModal"
                       tabIndex={-1}
@@ -148,19 +196,14 @@ const BookingRequest = () => {
                               aria-label="Close"
                             />
                           </div>
-                          {/* <div className="modal-body-test">
-                            <p className="text-primary-light p-1 fw-semibold text-md mb-0 mt-4">
-                              {customerproblem}
-                            </p>
-                          </div> */}
-                          {/* <div className="modal-body">
+                          <div className="modal-body">
                             <h6 className="text-primary-light fw-semibold text-md mb-0 mt-4">
                               {customerproblem}
                             </h6>
-                          </div> */}
+                          </div>
                         </div>
                       </div>
-                    </div>
+                    </div> */}
                   </td>
                   <td className="text-center">
                     <div className="d-flex justify-content-center gap-2">
