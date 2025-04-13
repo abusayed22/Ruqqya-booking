3d0dff65b621296b04a7dac7a4817dea792813ee
abu sayed <abusayed.in22@gmail.com>
2025-04-13 19:11:45 +0600

    test

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -2,5 +2,14 @@ import { handlers } from "@/auth";
 export const { GET, POST } = handlers;
 
 export const authOptions = {
-    trustHost: true, 
-  }
+  // REQUIRED: Whitelist your domains/IPs
+  trustHost: true,  // For development only
+  // OR for production:
+  hostWhitelist: [
+    '192.168.10.212', 
+    'localhost',
+    'yourdomain.com'
+  ],
+  
+  secret: process.env.NEXTAUTH_SECRET
+}
